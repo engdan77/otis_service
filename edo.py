49464779fe04e97c6b6c72f13d28b26791d5230a
edo <daniel@engvalls.eu>
2014-10-14 07:39:08 +0200

    Updating docs

--- edo.py
+++ edo.py
@@ -2,29 +2,9 @@
 
 # edo.py - This Module is for the most used classes and methods
 # URL: https://github.com/engdan77/edoautohome
-# Copyright (c) 2014
 # Author: Daniel Engvall (daniel@engvalls.eu)
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
-__version__ = "$Revision: 20141002.1099 $"
+__version__ = "$Revision: 20141014.1099 $"
 
 import sys
 import threading
